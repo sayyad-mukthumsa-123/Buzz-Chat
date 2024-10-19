b1cf56cd17a1e0da76e5acc52a1d9db257fc61c4
sayyad-mukthumsa-123 <sayyad.mukthumsa@gmail.com>
2024-10-19 17:53:57 +0530

    Development fix-3

--- .history/backend/server_20241014124028.js
+++ .history/backend/server_20241014124028.js
@@ -34,7 +34,7 @@ const server = app.listen(process.env.PORT, () =>
 );
 const io = socket(server, {
   cors: {
-    origin: ["http://localhost:3000","https://buzzchat-frontend-ilm3.onrender.com"],
+    origin: "http://localhost:3000",
     credentials: true,
   },
 });
